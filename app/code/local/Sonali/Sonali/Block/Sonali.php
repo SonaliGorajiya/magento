d888edb7e2ec5b2d6d4ece44c2dbf3c4c93e5d9e
SonaliGorajiya <gorajiyasonali@gmail.com>
2023-05-20 18:54:11 +0530

    Update a module with attribute-mgmt

---
+++ app/code/local/Sonali/Sonali/Block/Sonali.php
@@ -0,0 +1,8 @@
+<?php
+class Sonali_Sonali_Block_Sonali extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ app/code/local/Sonali/Sonali/Model/Sonali/Attribute.php
@@ -0,0 +1,8 @@
+<?php
+class Sonali_Sonali_Model_Sonali_Attribute extends Mage_Core_Model_Abstract
+{
+	function __construct()
+	{
+		$this->_init('sonali/sonali_attribute');
+	}
+}
