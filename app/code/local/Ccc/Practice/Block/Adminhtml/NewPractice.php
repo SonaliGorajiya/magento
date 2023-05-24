411d5659f3759ce746e7f842112abd2f81936162
SonaliGorajiya <gorajiyasonali@gmail.com>
2023-05-24 10:52:35 +0530

    16 to 20 tasks in Completed in 16,17,18,19 Tasks in Practice Module commit

---
+++ app/code/local/Ccc/Practice/Block/Adminhtml/NewPractice.php
@@ -0,0 +1,9 @@
+<?php
+ 
+class Ccc_Practice_Block_Adminhtml_NewPractice extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+    public function __construct()
+    {
+       echo "New Block";
+    }
+}
--- app/code/local/Ccc/Practice/controllers/IndexController.php
+++ app/code/local/Ccc/Practice/controllers/IndexController.php
@@ -1,10 +1,14 @@
-<?php 
+// Task : 19. How to overweite core or local controller action into local folder? Prepare 3 examples.
 
-class Ccc_Practice_IndexController extends Mage_Core_Controller_Front_Action
+<?php 
+require_once(Mage::getModuleDir('controllers','Ccc_Product').DS.'IndexController.php');
+// require_once('C:\xampp\htdocs\2023\magento\magento-mirror\app\code\local\Ccc\Product\controllers\IndexController.php');
+class Ccc_Practice_IndexController extends Ccc_Product_IndexController
 {
     public function indexAction()
     {
-        // echo "111";
+        echo '<pre>';
+        echo "123";
         echo '<pre>';
         // print_r(get_class_methods("Ccc_practice_IndexController"));
         // print_r(Mage::getModel('practice/practice')); 
