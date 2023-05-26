3f3d63f841b4ba5368ccbb95043dcbb76119a9d8
SonaliGorajiya <gorajiyasonali@gmail.com>
2023-05-26 11:20:04 +0530

    commit

--- app/code/local/Ccc/Practice/controllers/AdminHtml/NineteenController.php
+++ app/code/local/Ccc/Practice/controllers/AdminHtml/NineteenController.php
@@ -1,13 +1,16 @@
 // Task : 19. How to overweite core or local controller action into local folder? Prepare 3 examples.
 
 <?php 
-require_once('C:\xampp\htdocs\2023\magento\magento-mirror\app\code\local\Ccc\Product\controllers\AdminHtml/ProductController.php');
+// require_once(Mage::getModuleDir('controllers','Ccc_Product').DS.'adminhtml'.DS.'ProductController.php');
+
+require_once('C:\xampp\htdocs\2023\magento\magento-mirror\app\code\local\Ccc\Product\controllers\AdminHtml\ProductController.php');
 class Ccc_Practice_AdminHtml_NineteenController extends Ccc_Product_Adminhtml_ProductController
 {
     public function indexAction()
     {
-        // echo "111";
+        echo "111";
         echo '<pre>';
+        die;
         // print_r(get_class_methods("Ccc_practice_IndexController"));
         // print_r(Mage::getModel('practice/practice')); 
         // print_r($this->getLayout()->createBlock('practice/practice')); 
