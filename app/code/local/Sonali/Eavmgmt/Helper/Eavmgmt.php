cf94c09566decfe54a7a1d12dcd7b74fa6945bce
Gorajiya Sonali <91589075+SonaliGorajiya@users.noreply.github.com>
2023-05-30 10:51:57 +0530

    Update Eavmgmt.php

--- app/code/local/Sonali/Eavmgmt/Helper/Eavmgmt.php
+++ app/code/local/Sonali/Eavmgmt/Helper/Eavmgmt.php
@@ -1,6 +1,6 @@
 <?php
 
-class Sonali_Eavmgmt_Helper_Vendor extends Mage_Core_Helper_Abstract
+class Sonali_Eavmgmt_Helper_Eavmgmt extends Mage_Core_Helper_Abstract
 {
 	public function __construct()
 	{
